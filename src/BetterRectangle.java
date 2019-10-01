e166c9a12f7478d8496e5bb2b3ee1c356811b0a5
Aaron Jay Macaldo <aaronjay.macaldo@sjsu.edu>
2019-09-30 21:47:27 -0700

    Finished E9.13 and its tester

---
+++ src/BetterRectangle.java
@@ -0,0 +1,16 @@
+import java.awt.Rectangle;
+
+public class BetterRectangle extends Rectangle {
+    public BetterRectangle(int inWidth, int inHeight) {
+        super.setLocation(0, 0);
+        super.setSize(inWidth, inHeight);
+    }
+
+    public int getPerimeter() {
+        return this.height * 2 + this.width * 2;
+    }
+
+    public int getArea() {
+        return this.height * this.width;
+    }
+}
---
+++ src/BetterRectangleTester.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class BetterRectangleTester {
+
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+	    BetterRectangle userRectangle;
+        int userWidth = 0;
+	    int userHeight = 0;
+
+        System.out.println("Welcome to my program that creates a rectangle and returns the area and perimeter!");
+        System.out.println("Create a rectangle now.\nPlease enter the width: ");
+        userWidth = userInput.nextInt();
+        System.out.println("Please enter the height: ");
+        userHeight = userInput.nextInt();
+        userRectangle = new BetterRectangle(userWidth, userHeight);
+
+        System.out.println("\nYou have created a rectangle with width " + userRectangle.width + " and height " + userRectangle.height + "!");
+        System.out.println("The perimeter of your rectangle is: " + userRectangle.getPerimeter());
+        System.out.println("The area of your rectangle is: " + userRectangle.getArea());
+    }
+}
